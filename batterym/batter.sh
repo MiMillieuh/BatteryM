9b5745c78057eb564586e444d4d2c1beba979b65
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2024-01-22 16:17:28 +0100

    Add custom script support

--- batterym/batter.sh
+++ batterym/batter.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
 CURRENTPROFILE=$(powerprofilesctl get)
+BatteryFolder="/opt/batterym/onbatteryscripts"
+PowerFolder="/opt/batterym/onpowerscripts"
+StateFile="/opt/batterym/powerstate"
 
-if [[ $CURRENTPROFILE == "performance" ]]
-then
-exit 0
+if [[ $CURRENTPROFILE == "performance" ]]; then
+    exit 0
 fi
 
-POWERSTATE=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state")
+POWERSTATE=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "state" | awk '{print $2}')
 
-if [[ $POWERSTATE == "    state:               charging" || $POWERSTATE == "    state:               fully-charged" ]]
-then
-
-powerprofilesctl set balanced
-sudo cpupower frequency-set -g ondemand
-
+if [ -f "$StateFile" ]; then
+    PreviousState=$(cat "$StateFile")
 else
+    PreviousState=""
+fi
 
-powerprofilesctl set power-saver
-cpupower frequency-set -g powersave
+echo "$POWERSTATE" > "$StateFile"
+
+if [[ $POWERSTATE != $PreviousState ]]; then
+    if [[ $POWERSTATE == "charging" || $POWERSTATE == "fully-charged" ]]; then
+
+        powerprofilesctl set balanced
+        sudo cpupower frequency-set -g ondemand
+
+        for script in "$BatteryFolder"/*; do
+            if [ -x "$script" ]; then
+                echo "Executing $script"
+                "$script"
+            else
+                echo "$script Is not executable or not a file"
+            fi
+        done
+
+    else
+
+        powerprofilesctl set power-saver
+        cpupower frequency-set -g powersave
+
+        for script in "$PowerFolder"/*; do
+            if [ -x "$script" ]; then
+                echo "Executing $script"
+                "$script"
+            else
+                echo "$script Is not executable or not a file"
+            fi
+        done
+
+    fi
 fi
